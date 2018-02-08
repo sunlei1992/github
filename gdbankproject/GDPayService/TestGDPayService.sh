97f951fa1be7f8b333e6da72e00e1a39809b6187
sunlei1992 <314093901@qq.com>
2018-02-08 22:46:40 +0800

    Revert "gdbankprojectCode"
    This reverts commit 64edf8dd33b717bc5eb9b8343971f85102fc463c.

--- gdbankproject/GDPayService/TestGDPayService.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-java -Dfile.encoding="gb2312" -Dappname=GDPayService -Djava.ext.dirs=bin -cp bin  GDPayService.GDPayService
-
--- gdbankproject/GDPayService/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-javac -encoding "gb2312" -classpath ./src/log4j-1.2.17.jar:./src/dom4j.jar:./src/mysql-connector-java-5.1.27-bin.jar:./src/json.jar ./src/GDPayService/GDPayService.java ./src/GDPayService/Dbmanage.java ./src/GDPayService/SignatureUtil.java ./src/GDPayService/Configure.java
-mv ./src/GDPayService/*.class ./bin/GDPayService
